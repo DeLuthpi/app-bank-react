16533c4689e980d2aa0e71320833244b12f96f09
DeLuthpi <dedeluthpi006@gmail.com>
2024-08-25 10:48:11 +0700

    fix active sidebar navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import '../assets/css/sidebar-style.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { sidebarMenu } from "../helpers/const";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector, useDispatch } from 'react-redux';
@@ -8,8 +8,12 @@ import { menuActive } from "../redux/slices/menuSlice"
 const Sidebar = () => {
 	const isActive = useSelector(state => state.menu.pageActive);
 	const dispatch = useDispatch();
-	const handleActive = () => {
-		dispatch(menuActive());
+	const navigate = useNavigate();
+	const handleActive = (e) => {
+		e.preventDefault();
+		const path = e.target.pathname;
+		dispatch(menuActive(path));
+		navigate(e.target.pathname);
 	}
 
 	return (
@@ -19,7 +23,7 @@ const Sidebar = () => {
 					{sidebarMenu?.map((list) => {
 						return (
 							<li className="nav-item" key={list?.id}>
-								<Link className={`nav-link ${isActive === list?.navRoute ? "active" : ""}`} to={list?.navRoute} onClick={handleActive(list?.navRoute)}>
+								<Link className={`nav-link ${isActive === list?.navRoute ? "active" : ""}`} to={list?.navRoute} onClick={handleActive}>
 									<div className="icon icon-page">
 										<FontAwesomeIcon icon={list?.navIcon} />
 									</div>
--- src/redux/slices/menuSlice.js
+++ src/redux/slices/menuSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const menuSlice = createSlice({
 	name: "page",
 	initialState: {
-		pageActive: "home",
+		pageActive: "/home",
 	},
 	reducers: {
 		menuActive(state, action) {
